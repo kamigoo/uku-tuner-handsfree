e153ef7f52ac4b0e4e581faa98df3c33ac4cf3ce
kamigoo <95168166+kamigoo@users.noreply.github.com>
2021-12-03 22:01:56 +0100

    distance changes

--- main.ts
+++ main.ts
@@ -1,22 +1,18 @@
 let t = 0
 basic.showIcon(IconNames.EigthNote)
 basic.forever(function () {
-    t = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P2)
-    if (t >= 6 && t >= 8) {
+    t = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+    if (t >= 2 && t >= 8) {
         basic.showString("G")
         music.playTone(784, music.beat(BeatFraction.Double))
-        basic.pause(1000)
-    } else if (t > 8 && t >= 10) {
+    } else if (t > 8 && t >= 16) {
         basic.showString("C")
         music.playTone(262, music.beat(BeatFraction.Double))
-        basic.pause(1000)
-    } else if (t > 10 && t >= 12) {
+    } else if (t > 16 && t >= 24) {
         basic.showString("E")
         music.playTone(330, music.beat(BeatFraction.Double))
-        basic.pause(1000)
-    } else if (t > 12 && t >= 14) {
+    } else if (t > 24 && t >= 32) {
         basic.showString("A")
         music.playTone(440, music.beat(BeatFraction.Double))
-        basic.pause(1000)
     }
 })
